b336449b5611246b63377038469f774bedccf08c
Sudip Das <sudip5428@gmail.com>
2022-10-31 22:50:14 +0530

    code commit for docker file add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template, request, url_for, redirect, session
 # im
 # port pickle
 # production server
-from livereload import Server
+# from livereload import Server
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def pradict():
     return render_template('index.html',pradiction_test="twitter status is ={}".format())
 
 if __name__ == "__main__":
-#   app.run(debug=True, host='0.0.0.0', port=5000)
+  app.run(debug=True, host='0.0.0.0', port=5000)
     # production
-    server = Server(app.wsgi_app)
-    server.serve(port=5000)
+    # server = Server(app.wsgi_app)
+    # server.serve(port=5000)
