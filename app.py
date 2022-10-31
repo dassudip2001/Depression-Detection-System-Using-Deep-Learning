1d6079f334eecba48992a3328007211575477c46
Sudip Das <sudip5428@gmail.com>
2022-10-31 22:41:11 +0530

    python file add and k8s config cheanges

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask,render_template, request, url_for, redirect, session
+# im
+# port pickle
+# production server
+from livereload import Server
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-
+# model=pickle.load(open("model.pkl","rb"))
+@app.route("/pradict",methods=["POST"])
+def pradict():
+    return render_template('index.html',pradiction_test="twitter status is ={}".format())
 
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0', port=5000)
+#   app.run(debug=True, host='0.0.0.0', port=5000)
+    # production
+    server = Server(app.wsgi_app)
+    server.serve(port=5000)
---
+++ my_twitter.py
@@ -0,0 +1,42 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+import pickle
+tweet = input("Enter the value:")
+
+
+# precprcess_tweet
+tweet_words = []
+
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+
+    elif word.startswith('http'):
+        word = "http"
+    tweet_words.append(word)
+
+tweet_proc = " ".join(tweet_words)
+
+# load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ['Negative', 'Neutral', 'Positive']
+
+# sentiment analysis
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+# output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+output = model(**encoded_tweet)
+
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+for i in range(len(scores)):
+    l = labels[i]
+    s = scores[i]
+    print(l,s)
+
+# Transfer to pkl file
+pickle.dump(model,open("model.pkl","wb"))
